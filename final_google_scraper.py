2bb30f1cc2a56427f2ed445cf26127860b557a27
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2022-02-13 17:40:08 +0530

    Add files via upload

---
+++ final_google_scraper.py
@@ -0,0 +1,71 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import requests
+import io
+from PIL import Image
+import time
+
+PATH = "chromedriver"
+# location of the chromedriver
+wd = webdriver.Chrome(PATH)
+wd.maximize_window()
+def get_images_from_google(wd, delay, max_images):
+	def scroll_down(wd):
+		wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+		wd.implicitly_wait(10)
+        
+
+	url = "https://www.google.com/search?q=white cats&tbm=isch&ved=2ahUKEwjykJ779tbzAhXhgnIEHSVQBksQ2-cCegQIABAA&oq=cats&gs_lcp=CgNpbWcQAzIHCAAQsQMQQzIHCAAQsQMQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzoHCCMQ7wMQJ1C_31NYvOJTYPbjU2gCcAB4AIABa4gBzQSSAQMzLjOYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=7vZuYfLhOeGFytMPpaCZ2AQ&bih=817&biw=1707&rlz=1C1CHBF_enCA918CA918"
+	wd.get(url)
+
+	image_urls = set()
+	skips = 0
+
+	while len(image_urls) + skips < max_images:
+		scroll_down(wd)
+
+		thumbnails = wd.find_elements(By.CLASS_NAME, "Q4LuWd")
+
+		for img in thumbnails[len(image_urls) + skips:max_images]:
+			try:
+				img.click()
+				time.sleep(delay*2)
+			except:
+				continue
+
+			images = wd.find_elements(By.CLASS_NAME, "n3VNCb")
+			for image in images:
+				if image.get_attribute('src') in image_urls:
+					max_images += 1
+					skips += 1
+					break
+
+				if image.get_attribute('src') and 'http' in image.get_attribute('src'):
+					image_urls.add(image.get_attribute('src'))
+					print(f"Found {len(image_urls)}")
+
+	return image_urls
+
+
+def download_image(download_path, url, file_name):
+	try:
+		image_content = requests.get(url).content
+		image_file = io.BytesIO(image_content)
+		image = Image.open(image_file)
+		file_path = download_path + file_name
+
+		with open(file_path, "wb") as f:
+			image.save(f, "JPEG")
+
+		print("Success")
+	except Exception as e:
+		print('FAILED -', e)
+
+urls = get_images_from_google(wd,1,10)
+#do not increase the range very high 
+#dont forget to craeat a folder name of google_scrape_image in same place where you put your code...
+for i, url in enumerate(urls):
+	download_image("google_scrape_image/", url, str(i) + ".jpg")
+
+print("task complete")
+wd.quit()
